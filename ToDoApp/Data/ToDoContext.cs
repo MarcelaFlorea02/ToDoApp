33046789a194cf6f724b61ffc447424e3a55c6b8
marcelaflorea02 <marcela.florea02@gmail.com>
2026-04-15 09:53:25 +0300

    add DbContext and Controller

---
+++ ToDoApp/Data/ToDoContext.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoApp.Models;
+
+namespace ToDoApp.Data;
+
+public class ToDoContext : DbContext
+{
+    public ToDoContext(DbContextOptions<ToDoContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<ToDoItem> ToDoItems { get; set; }
+
+    //get all items 
+    public Task<List<ToDoItem>> GetAllAsync()
+    {
+        var items = ToDoItems.AsQueryable().ToListAsync();
+        return items;
+    }
+
+    //get item by id 
+    public Task<ToDoItem> GetByIdAsync(int id)
+    {
+        var item = ToDoItems.Where(i => i.Id == id).FirstOrDefaultAsync();
+        if (item == null)
+            throw new Exception("Item not found");
+        return item;
+    }
+
+    //add new item 
+    public async Task AddAsync(ToDoItem item)
+    {
+        await ToDoItems.AddAsync(item);
+        await base.SaveChangesAsync();
+    }
+
+    //update item 
+    public async Task UpdateAsync(ToDoItem item)
+    {
+        ToDoItems.Update(item);
+        await base.SaveChangesAsync();
+    }
+
+    //delete an item 
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var item = await ToDoItems.Where(i => i.Id == id).FirstOrDefaultAsync();
+        if (item == null)
+            return false;
+
+        ToDoItems.Remove(item);
+        await base.SaveChangesAsync();
+        return true;
+    }
+
+
+
+}
---
+++ ToDoApp/Models/ToDoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoApp.Models;
+
+public class ToDoItem
+{
+    public int Id { get; set; }
+    public required string Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public bool IsDone { get; set; }
+}
